8b1e2ead8c6bdcc8645f5a020b455b03f1b23d67
Andre William <andrecool.cool@gmail.com>
2021-11-06 22:08:42 +0700

    top section

---
+++ src/components/responsive/index.ts
@@ -0,0 +1,7 @@
+export const deviceSize = {
+  mobile: 640,
+  tablet: 768,
+  laptop: 1024,
+  dekstop: 1280,
+  widescreen: 1536,
+};
---
+++ src/containers/TopSection/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { NavBar } from '../../components/NavBar';
+import BackgroundImage from '../../images/indonesia-high-res.jpg';
+
+const TopSectionContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-col
+    h-screen
+    relative
+  `}
+`;
+
+const LandingSection = styled.div`
+  ${tw`
+    w-full
+    h-screen
+    flex
+    flex-col
+  `};
+  background-image: url(${BackgroundImage}), linear-gradient(to left, #005b9c, #b1d1b148);
+  background-size: cover;
+  background-position: bottom 10% left;
+  background-blend-mode: overlay;
+`;
+
+export function TopSection() {
+  return (
+    <TopSectionContainer>
+      <LandingSection>
+        <NavBar />
+      </LandingSection>
+    </TopSectionContainer>
+  );
+}
